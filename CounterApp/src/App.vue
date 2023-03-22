ce1cdccbcc784ec1f4af0fb1b9bc93ee5834577f
Julio Duarte <73315417+maoiki@users.noreply.github.com>
2023-03-22 15:30:10 -0300

    alteracoes no estilo em counterapp

--- CounterApp/src/App.vue
+++ CounterApp/src/App.vue
@@ -1,36 +1,57 @@
 <script setup>
-import {ref} from 'vue';
+import {ref} from 'vue'
 
-const count = ref(0);
+const count = ref(0)
 const addToCounter = () => {
-  count.value = count.value + 1
+  count.value += 1
 }
 const subtractFromCounter = () => {
-  count.value = count.value - 1
+  count.value -= 1
 }
+;
 </script>
 
 <template>
   <main class="main_content">
     <div class="content">
       <h1>The current count is: </h1>
-      <h2> {{ count }}</h2>
-      <!-- ddd --> 
+      <p> {{ count }}</p>
       <button @click="subtractFromCounter">-</button>
       <button @click="addToCounter">+</button>
+      <footer><a class="link_externo" href="https://github.com/maoiki" target="_blank">Feito com ♥ por  Julio Duarte</a></footer>
     </div>
   </main>
 </template>
 
 <style scoped>
   .main_content {
-  /* No final de cada conjunto chave-valor é necessário adicionar o ponto e vírgula. */
+    height: 100vh;
     display: flex;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
-    background-color: rgb(24, 141, 106);
-  } /* Não precisa colocar vírgula entre os seletores diferentes, basta por um abaixo do outro. */
+  } 
   .content {
     text-align: center;
+    width:fit-content;
+    padding: 40px;
+    border-radius: 50px;
+    background-color: #41b883;
+  }
+  
+  button {
+    font-size: 20px;
+    height: 35px;
+    width: 35px;
+    margin: 5px 5px 35px 10px;
+  }
+
+  p{
+    font-size:40px;
+  }
+  a {
+    align-self: end;
+    text-decoration: none;
+    color: var(--color-text);
   }
 </style>
